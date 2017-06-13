72738def85faa86facd45c489652eea35413b272
Ritwik Bhaduri <ritwik.bhaduri@gmail.com>
2017-06-14 00:15:38 +0530

    Create k means my code.R

--- k means my code.R
+++ k means my code.R
@@ -1,47 +1,38 @@
-distance <- function(vector1, vector2) {
-    d = 0
-    for (i in 1:length(vector1)) d = d + (vector1[i] - vector2[i])^2
-    sqrt(d)
-}
-euclid <- function(vector1, vector2) {
-    distancematrix = matrix(nrow = nrow(vector1), ncol = nrow(vector2))
-    for (i in 1:nrow(distancematrix)) 
-	  for (j in 1:ncol(distancematrix)) distancematrix[i,j] = distance(vector1[i,],vector2[j,])
-    return(distancematrix)
-}
 Kmeans <- function(x, n, min = 0, max) {
-    x = as.matrix(x)
-    centernumbers = sample(nrow(x), n)
-    centers = matrix(nrow = n, ncol = ncol(x))
-    centers = x[centernumbers, ]
-    flag = 0
-    repeat {
-        dist = euclid(x, centers)
-        cluster = matrix(nrow=nrow(x),ncol=1)
-        for (i in 1:nrow(x)) cluster[i,1] = which.min(euclid(x, centers)[i,])
-        newcenters = matrix(0, nrow = n, ncol = ncol(x))
-        count = matrix(0, nrow = n, ncol = 1)
-        for (i in 1:nrow(x)) {
-            newcenters[cluster[i,1],] = newcenters[cluster[i,1],] + x[i,]
-            count[cluster[i, 1]] = count[cluster[i, 1]] + 1
-        }
-        for (i in 1:n) newcenters[i, ] = newcenters[i, ]/count[i]
-        d = 0
-        for (i in 1:n) d = d + distance(centers[i, ], newcenters[i,])
-        centers = newcenters
-        flag = flag + 1
-        if (missing(min)){ if(flag > max)	break} 
-        else	{if (d < min) break }
-    }
-    return(list(cluster = cluster, centers = centers))
+       centernumbers = sample(nrow(x), n)
+       centers = matrix(nrow = n, ncol = ncol(x))
+       centers = x[centernumbers, ]
+       flag = 0
+       repeat {
+               distance=matrix(nrow=nrow(x),ncol=n)
+               for(i in 1:nrow(x)){
+                     for(j in 1:n){
+                           distance[i,j]=t(x[i,]-centers[j,])%*%(x[i,]-centers[j,])
+                     }
+               }
+               cluster=as.matrix(apply(distance,1,which.min))
+               newcenters = matrix(0, nrow = n, ncol = ncol(x))
+               count = matrix(0, nrow = n, ncol = 1)
+               for (i in 1:nrow(x)) {
+                      newcenters[cluster[i,1],] = newcenters[cluster[i,1],] + x[i,]
+                      count[cluster[i, 1]] = count[cluster[i, 1]] + 1
+               }
+               for (i in 1:n) newcenters[i, ] = newcenters[i, ]/count[i]
+               d = 0
+               for (i in 1:n) d = d + t(centers[i,]-newcenters[i,])%*%(centers[i,]-newcenters[i,])
+               centers = newcenters
+               flag = flag + 1
+               if (missing(min)){ if(flag > max)	break} 
+               else	{if (d < min) break }
+       }
+       return(list(cluster = cluster, centers = centers,iterations=flag))
 }
 #x=as.matrix(read.table("D:\\sagatam das machine learning\\flame.txt"))[,c(1,2)] 
 #x=as.matrix(read.table("D:\\sagatam das machine learning\\R15.txt"))[,c(1,2)]
 #x = read.table("clustering.txt")
-#x=volcano[,c(1,51)]
 x=as.matrix(iris[,c(3,4)])
-r = Kmeans(x,2,min=10^-3 , max = 10)
-r$centers
-r
+r = Kmeans(x,3,min=10^-5, max = 10)
+#r$centers
+r$iterations
 plot(x, col = r$cluster, pch = 16)
 points(r$centers, col = 1:3, pch = 8, cex = 2)
